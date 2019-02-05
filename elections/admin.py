687719123041bac9784c0fe62e2b99b5ae0c569d
Anirudh Rowjee <ani.rowjee@gmail.com>
2019-02-05 13:12:06 +0530

    Added Elections Model

---
+++ elections/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+
+admin.site.register(models.Election)
+admin.site.register(models.Posts)
+admin.site.register(models.VoterTypes)
---
+++ elections/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+import datetime
+# Create your models here.
+
+
+class Election(models.Model):
+    current_year = datetime.date.today().year
+    now = datetime.datetime.now
+
+    year = models.IntegerField(default=current_year)
+    election_number = models.IntegerField(default=1)
+    is_active = models.BooleanField(default=False)
+    type = models.CharField(default='General Election', max_length=50)
+    created_timestamp = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (('year', 'election_number'),)
+
+    def __str__(self):
+        name = self.type + ' ' + str(self.year)
+        return name
+
+
+class Posts(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'posts'
+
+
+class VoterTypes(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    value = models.IntegerField(default=1)
+
+    class Meta:
+        verbose_name_plural = 'Voter Types'
+
+    def __str__(self):
+        return self.name
+
